64ca80ced00bd6db9b63fa7f738c96d69dcc11c6
alextanhongpin <alex.tan@bandlab.com>
2025-04-12 11:58:10 +0800

    fix: use openai endpoints

--- 001_quickstart/multi_tool_agent/agent.py
+++ 001_quickstart/multi_tool_agent/agent.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
@@ -46,10 +47,16 @@ def get_current_time(city: str) -> dict:
     return {"status": "success", "report": f"The current time in {city} is {now}."}
 
 
+# TODO: native openai https://github.com/google/adk-python/issues/27
+os.environ["OPENAI_API_BASE"] = os.getenv("OPENAI_BASE_URL")
+
+model = os.getenv('CHAT_MODEL', 'llama3.2')
 root_agent = Agent(
     name="weather_time_agent",
-    # model=LiteLlm(model="ollama/gemma3:4b"),  # Standard LiteLLM format for Ollama
-    model=LiteLlm(model="ollama/llama3.2"),  # Standard LiteLLM format for Ollama
+    # https://github.com/google/adk-python/issues/49
+    # model=LiteLlm(model="ollama_chat/gemma3:4b"),  # Standard LiteLLM format for Ollama
+    # model=LiteLlm(model="ollama_chat/llama3.2"),  # Standard LiteLLM format for Ollama
+    model=LiteLlm(model="openai/" + model),  # Standard LiteLLM format for Ollama
     description=("Agent to answer questions about the time and weather in a city."),
     instruction=("I can answer questions about the time and weather in a city."),
     tools=[get_weather, get_current_time],
