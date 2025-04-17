cbecb794f34100ff6fd8cf203c9d90a590eed60b
alextanhongpin <alex.tan@bandlab.com>
2025-04-17 20:07:05 +0800

    style: format

--- 001_quickstart/roulette_wheel/agent.py
+++ 001_quickstart/roulette_wheel/agent.py
@@ -11,7 +11,8 @@ def roulette_wheel(square: int) -> str:
     Returns:
         str: Returns 'winner' if the square is 18, otherwise 'loser'.
     """
-    return 'winner' if square == 18 else 'loser'
+    return "winner" if square == 18 else "loser"
+
 
 # TODO: native openai https://github.com/google/adk-python/issues/27
 os.environ["OPENAI_API_BASE"] = os.getenv("OPENAI_BASE_URL")
@@ -21,6 +22,8 @@ root_agent = Agent(
     name="roulette_wheel_agent",
     model=LiteLlm(model="openai/" + model),  # Standard LiteLLM format for Ollama
     description=("Agent to check if the user is a winner in roulette."),
-    instruction=("Use the roulette_wheel function to check if the user is a winner based on the number they provide."),
+    instruction=(
+        "Use the roulette_wheel function to check if the user is a winner based on the number they provide."
+    ),
     tools=[roulette_wheel],  # List of tools the agent can use
 )
